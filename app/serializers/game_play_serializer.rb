3228e44e120b565860f34dd8ee599c71f6420c3c
Ellie <ellie.staniforth@coverwallet.com>
2021-04-05 21:06:36 +0200

    add time stamps

--- app/serializers/game_play_serializer.rb
+++ app/serializers/game_play_serializer.rb
@@ -1,16 +1,20 @@
 class GamePlaySerializer < ActiveModel::Serializer
-    attributes :moves, :result
+  attributes :moves, :result, :timestamps
 
-    def moves
-        [
-            {
-              'name': object.guest_player,
-              'move': object.guest_move
-            },
-            {
-              'name': 'bot',
-              'move': object.computer_move
-            }
-          ]
-    end
+  def moves
+      [
+          {
+            'name': object.guest_player,
+            'move': object.guest_move
+          },
+          {
+            'name': 'bot',
+            'move': object.computer_move
+          }
+        ]
+  end
+
+  def timestamps
+    object.created_at.to_formatted_s(:short)  
+  end
 end
